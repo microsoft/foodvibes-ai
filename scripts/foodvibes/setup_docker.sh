791c22611bcd57a178f908ad2e4c3e3cffb0d6e5
Cyrus Kasra (Sophus It Solutions LLC) <v-cyruskasra@microsoft.com>
2024-09-12 23:58:28 +0000

    Changed setup_docker logic to pick up existing exited container (if any)

--- scripts/foodvibes/setup_docker.sh
+++ scripts/foodvibes/setup_docker.sh
@@ -37,42 +37,64 @@ export DOCKER_CLI_HINTS=false
 
                 logger 0 "Checking/starting Docker container for image \"$image_name\"..."
 
-                typeset container_id=$(docker ps | grep $image_name | cut -d' ' -f1)
+                typeset container_id=$(docker ps -a --filter "status=running" | grep $image_name | head -1 | cut -d' ' -f1)
+                typeset container_exists=0
+                typeset container_is_active=0
                 typeset cmd="setup.sh"
                 typeset msg="to complete FoodVibes.ai installation and startup"
                 typeset docker_exec="docker exec"
 
                 [[ "$(env | grep TERM_PROGRAM= | cut -d= -f2)" == "mintty" ]] &&
-                {
-                    logger 0 "Found mintty, using winpty with \"${docker_exec}\"..."
-                    docker_exec="winpty ${docker_exec}"
-                }
+                    {
+                        logger 0 "Found mintty, using winpty with \"${docker_exec}\"..."
+                        docker_exec="winpty ${docker_exec}"
+                    }
 
-                if [[ -z "$container_id" ]]; then
-                    logger 0 "No containers are running. Starting container..."
-
-                    port_be=$(get_value "7478" 0 "Backend port # to use")
-                    port_fe=$(get_value "3000" 0 "Frontend port # to use")
-                    set_env_file_entry $key_name_port_be $port_be $env_file &&
-                        set_env_file_entry $key_name_port_fe $port_fe $env_file &&
-                        run_cmd docker run --name "$image_name-$(truncate_unique_id)" --init --privileged -p ${port_fe}:3000 -p ${port_be}:7478 -d $image_name &&
-                        {
-                            rc=0
-                            container_id=$(docker ps | grep $image_name | cut -d' ' -f1)
-                        }
+                if [[ -n "$container_id" ]]; then
+                    container_exists=1
+                    container_is_active=1
                 else
+                    container_id=$(docker ps -a --filter "status=exited" | grep $image_name | head -1 | cut -d' ' -f1)
+                    [[ -n "$container_id" ]] && container_exists=1
+                fi
+
+                if (($container_is_active == 1)); then
                     logger 1 "Found running container \"$container_id\""
                     rc=0
+                else
+                    logger 0 "No containers are running"
 
-                    eval $docker_exec -it $container_id bash -c \'grep RESOURCE_GROUP_NAME /app/.env \>/dev/null 2\>\&1\' &&
-                        {
-                            cmd="setup_launch.sh"
-                            msg="to restart foodvibes-ai"
-                        }
+                    if (($container_exists == 1)); then
+                        logger 0 "Restarting exited container \"$container_id\"..."
+
+                        run_cmd docker restart $container_id
+                        rc=$?
+                    else
+                        logger 0 "Starting new container..."
+
+                        port_be=$(get_value "7478" 0 "Backend port # to use")
+                        port_fe=$(get_value "3000" 0 "Frontend port # to use")
+
+                        set_env_file_entry $key_name_port_be $port_be $env_file &&
+                            set_env_file_entry $key_name_port_fe $port_fe $env_file &&
+                            run_cmd docker run --name "$image_name-$(truncate_unique_id)" --init --privileged -p ${port_fe}:3000 -p ${port_be}:7478 -d $image_name &&
+                            {
+                                rc=0
+                                container_id=$(docker ps | grep $image_name | head -1 | cut -d' ' -f1)
+                            }
+                    fi
+
+                    (($rc == 0)) && container_is_active=1
                 fi
 
                 (($rc == 0)) &&
                     {
+                        eval $docker_exec -it $container_id bash -c \'grep RESOURCE_GROUP_NAME /app/.env \>/dev/null 2\>\&1\' &&
+                            {
+                                cmd="setup_launch.sh"
+                                msg="to restart foodvibes-ai"
+                            }
+
                         rc=1
                         logger 1 "Docker container is running. Run\n\n    $cmd\n\n$msg"
                         run_cmd_conditional_check 1 $docker_exec -it $container_id bash -c 'bash -l'
